9526f96d368f738eb79ebf988a030e6cd3970159
Shibnath <63232219+ShibnathMaji@users.noreply.github.com>
2023-08-24 10:41:39 +0530

    Commit 7 Added Functionalities: 1. Ability to fetch Quiz Questions by sending the Quiz ID. 2. Will only return the list of Question IDs, Question Titles and the Options for the Questions in response.

--- Quiz-WebApp-Project/src/main/java/com/example/quizwebapp/controller/QuizController.java
+++ Quiz-WebApp-Project/src/main/java/com/example/quizwebapp/controller/QuizController.java
@@ -1,11 +1,17 @@
 package com.example.quizwebapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.quizwebapp.model.QuestionWrapper;
 import com.example.quizwebapp.service.QuizService;
 
 @RestController
@@ -34,4 +40,19 @@ public class QuizController
 	{
 		return quizService.createQuiz(category, numQ, title);
 	}
+	
+	//----------------------------------------------------------------------------	
+	
+	/* For accepting requests in the path-> localhost:8080/quiz/get/<preferred_id>
+	 * Here, we are accepting the QuizID associated with the QuizTitle in the DB from the user.
+	 * Templated part of the URL gets mapped to the Integer id.
+	 * Returns the list of questions in the Quiz.
+	 * It only returns the Question ID, Question Title and the Options 1-4 for each of the questions. 
+	 */
+	@GetMapping("get/{id}")
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestionsByQuizTitle(@PathVariable ("id") Integer id)
+	{
+		return quizService.getQuizQuestions(id);
+	}
+	
 }
--- Quiz-WebApp-Project/src/main/java/com/example/quizwebapp/service/QuizService.java
+++ Quiz-WebApp-Project/src/main/java/com/example/quizwebapp/service/QuizService.java
@@ -1,6 +1,8 @@
 package com.example.quizwebapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.example.quizwebapp.dao.QuestionDAO;
 import com.example.quizwebapp.dao.QuizDAO;
 import com.example.quizwebapp.model.Question;
+import com.example.quizwebapp.model.QuestionWrapper;
 import com.example.quizwebapp.model.Quiz;
 
 @Service
@@ -37,6 +40,46 @@ public class QuizService
 		quizDAO.save(quiz);
 		
 		return new ResponseEntity<String>("Success", HttpStatus.CREATED);
+	}
+	
+	//----------------------------------------------------------------------------	
+	
+	/* This method takes the QuizID as input, finds the quiz instance associated with the ID
+	 * and returns the questions in the Quiz.
+	 * Each of the Questions in the quiz are of the type -> Question.java [Entity/Model class]
+	 * This function converts that into -> QuestionWrapper.java [Entity/Model class]
+	 * Reason for this conversion is:
+	 * We don't need to send all the attributes mentioned in the Question.java to the user
+	 * We only need to send Question ID, Question Title and the Options 1-4.
+	 * 
+	 * QuestionWrapper.java has only the above mentioned attributes and we send the values
+	 * to a parameterized constructor.
+	 */
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) 
+	{
+		/* Optional return type signifies that we might not get a response in case the id is not present.
+		 * Here, we are storing a quiz instance associated with the id. .
+		 */
+		Optional<Quiz> quiz = quizDAO.findById(id);
+		
+		/* questionFromDB extract the List of Questions present in the quiz instance. 
+		 * get() is used because quiz's return type is Optional
+		 * Defining questionForUser as an ArrayList. It'll contain objects of type QuestionWrapper.
+		 */ 
+		List<Question> questionFromDB = quiz.get().getQuestions();
+		List<QuestionWrapper>questionForUser = new ArrayList<QuestionWrapper>();
+		
+		// Converting Question object to QuestionWrapper object
+		for(Question q: questionFromDB)
+		{
+			//Generating a new QuestionWrapper instance
+			QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), 
+					q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+			
+			//Adding this instance into the ArrayList questionForUser
+			questionForUser.add(qw);
+		}
+		return new ResponseEntity<>(questionForUser, HttpStatus.OK);
 	}
 
 }
